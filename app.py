69cc3d31c7f27967ba7542e22eea62ad9f5e1a72
1725110441-Yair <1725110441@utectulancingo.edu.mx>
2026-09-16 19:34:05 +0000

    UPDATED

---
+++ app.py
@@ -0,0 +1,14 @@
+import web
+
+urls = (
+    '/', 'controllers.index.Index',
+    '/lista_contactos','controllers.lista_contactos.ListaContactos',
+    '/ver_contacto/(.+)', 'controllers.ver_contacto.VerContacto',
+    '/editar_contacto/(.+)', 'controllers.editar_contacto.EditarContacto',
+    '/borrar_contacto/(.+)', 'controllers.borrar_contacto.BorrarContacto',
+    '/insertar_contacto','controllers.insertar_contacto.InsertarContacto'
+)
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
